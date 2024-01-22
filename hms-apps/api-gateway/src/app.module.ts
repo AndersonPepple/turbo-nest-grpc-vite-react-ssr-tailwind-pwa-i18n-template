1e5dd450179f9b03dfa4226a3d51a29dfca37023
AndersonPepple <anderson.pepple@pau.edu.ng>
2024-01-22 01:07:54 +0100

    My Assignment

--- hms-apps/api-gateway/src/app.module.ts
+++ hms-apps/api-gateway/src/app.module.ts
@@ -1,3 +1,4 @@
+/*
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -15,3 +16,27 @@ import { UsersModule } from './users/users.module';
   providers: [AppService],
 })
 export class AppModule {}
+*/
+
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EmployeesModule } from './employees/employees.module';
+import { PatientsModule } from './patients/patients.module';
+import { ClinicsModule } from './clinics/clinics.module';
+import { PharmaciesModule } from './pharmacies/pharmacies.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join} from 'path';
+
+
+@Module({
+  imports: [ServeStaticModule.forRoot({rootPath: join(__dirname, '../../..', 'users-demo-frontend','dist'),}),
+            EmployeesModule, PatientsModule, ClinicsModule, PharmaciesModule, AuthModule, UsersModule
+    ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
