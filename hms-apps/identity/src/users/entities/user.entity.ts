01f2246c37c7d838193c0ba350213d366441583a
AndersonPepple <anderson.pepple@pau.edu.ng>
2024-02-20 16:34:22 +0100

    updated the user entity

--- hms-apps/identity/src/users/entities/user.entity.ts
+++ hms-apps/identity/src/users/entities/user.entity.ts
@@ -1,19 +1,34 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
-      @PrimaryGeneratedColumn('uuid')
-      id: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-      @Column()
-      primaryEmailAddress: string;
+  @Column({ unique: true })
+  primaryEmailAddress: string;
 
-      @Column()
-      passwordHash: string;
+  @Column()
+  firstName: string;
 
-      @Column()
-      firstName: string;
-      
-      @Column()
-      lastName: string;
+  @Column({ nullable: true })
+  middleName?: string | undefined;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  passwordHash: string;
+
+  @Column()
+  backupEmailAddress: string;
+
+  @Column("json", { nullable: true })
+  phone: object | null;
+
+  @Column({ default: false })
+  isPrimaryEmailAddressVerified: boolean;
+
+  @Column({ default: false })
+  isBackupEmailAddressVerified: boolean;
 }
